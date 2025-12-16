d4850d56de151e253ec70f2bc95a35b0590acac7
laminesdev <aminesdev@proton.me>
2025-12-16 17:35:05 +0100

    adding posthug

--- app/components/ExploreBtn.tsx
+++ app/components/ExploreBtn.tsx
@@ -2,15 +2,18 @@
 import Image from "next/image";
 
 const ExploreBtn = () => {
+
    return (
-      <button
-         type="button"
-         id="explore-btn"
-         className="mt-7 mx-auto"
-      >
+      <button type="button" id="explore-btn" className="mt-7 mx-auto">
          <a href="#events">
             <h2>Explore Events</h2>
-            <Image src="/icons/arrow-down.svg" alt="arrow-down" width={16} height={16} className="ml-5"/>
+            <Image
+               src="/icons/arrow-down.svg"
+               alt="arrow-down"
+               width={16}
+               height={16}
+               className="ml-5"
+            />
          </a>
       </button>
    );
---
+++ lib/instrumentation-client.ts
@@ -0,0 +1,8 @@
+import posthog from "posthog-js";
+
+if (typeof window !== "undefined") {
+   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+      autocapture: true,
+   });
+}
